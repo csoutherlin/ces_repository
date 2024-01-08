c14801e48d548bb451b551c6d38e6c85326d816a
csoutherlin <southerlinc@outlook.com>
2024-01-08 09:12:22 -0700

    YOUR COMMIT MESSAGE

---
+++ shapes.py
@@ -0,0 +1,18 @@
+#area of TRIANGLE
+#a = float(input('Enter first side: '))
+#b = float(input('Enter second side: '))
+#c = float(input('Enter third side: '))
+#s = (a + b + c) / 2
+#area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+#print(area)
+
+#area of SQUARE
+#length = float(input('Enter the length: '))
+#width = float(input('Enter the width'))
+#area = length * width
+#print(area)
+
+#area of a CIRCLE
+r = float(input('Enter the radius: '))
+area = 3.14 * (r ** 2)
+print(area)
