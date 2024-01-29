4d1a04ba3c0749683c1f5736cd985f457e4cdca4
Christopher Southerlin <155988600+csoutherlin@users.noreply.github.com>
2024-01-29 10:29:18 -0700

    Add files via upload

---
+++ phone.py
@@ -0,0 +1,8 @@
+phone_book = {
+    "Alice": "555-1234",
+    "Bob": "555-5678",
+    "Charlie": "555-2468"
+}
+
+
+print(f"The number of Alice is {phone_book['Alice']}")
---
+++ transaction.py
@@ -0,0 +1,18 @@
+transaction1 = 10.20
+transaction2 = 31.32
+transaction3 = 5
+
+tranactions = [10.2, 31.32, 5]
+#tranactions.append(7.82) - dynamically add item to list
+#tranactions.pop() # remove last item in list
+
+#print(tranactions)
+#print(tranactions[0]) # indexing - print first item on list
+#print(tranactions[-1]) # indexing - print last item on list...
+#print(tranactions[len(tranactions) -1])
+#print(tranactions[0])
+
+#tranactions.append(40.8) #adds to end of list
+#tranactions.insert(1, 40.8) #insert 40.8 into indice #1 position
+tranactions.pop(2) #remove indice position #2
+print(tranactions)
